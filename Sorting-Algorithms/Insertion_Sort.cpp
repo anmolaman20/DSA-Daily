132e894d18d82abde5352145b0fd7c42b9afdd3f
anmolaman20 <anmolaman8853@gmail.com>
2023-06-27 17:01:35 +0530

    Code of Insertion-Sort added with cpp implementation

---
+++ Sorting-Algorithms/Insertion_Sort.cpp
@@ -0,0 +1,33 @@
+//Not - in Place
+//Stable
+//O(n) -> Best, O(n^2) -> Avg
+//Online
+//Comparision Based Sorting ALgorithms
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void Insertion_sort(vector<int> &vec){
+    int n = vec.size();
+    for(int i=1;i<n;i++){
+        int key = vec[i];
+        int j = i-1;
+        while(j>=0 and vec[j] > key){
+            swap(vec[j+1],vec[j]);
+            j--;
+        }
+        vec[j+1] = key;
+    }
+    
+}
+void print(vector<int> &vec){
+    for(const auto& it : vec){
+        cout<<it<<" ";
+    }
+}
+int main(){
+    vector<int> vec = {3,4,1,2,5,6,7,8,1};
+    Insertion_sort(vec);
+    print(vec);
+return 0;
+}
